e78d716bf07d3b61dae3379b2635123967f75d99
kopanitsa <okada.takahiro111@gmail.com>
2012-02-19 13:19:44 +0900

    remove unused Activity and add Game class to control Game

---
+++ src/com/pigmal/android/ex/accessory/Game.java
@@ -0,0 +1,16 @@
+package com.pigmal.android.ex.accessory;
+
+public class Game {
+
+    enum GameMode {
+        start,
+        zyanken,
+        acchimuite_hoi,
+        celemony, // win or lose
+    };
+    
+    public void setGameMode(GameMode mode){
+        
+    }
+    
+}
